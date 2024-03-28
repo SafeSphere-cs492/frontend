4f7662287f0d5717d5e262adc0054e01b72b534b
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-03-28 08:13:51 -0400

    integrated socket.io to display speech in frontend

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { io } from "socket.io-client";
 
 function App() {
   const [data, setData] = useState("");
-
-  const handleTranscriptionStart = () => {
-    // Assuming your backend is running on localhost:3000 and endpoint as /api/audio/transcribe-audio
-    const apiUrl = "http://localhost:3000/api/audio/transcribe-audio";
-
-    fetch(apiUrl, { method: "POST" })
-      .then((response) => response.text())
-      .then((data) => {
-        console.log("Success:", data);
-        // Handle success response
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        // Handle error response
-      });
-  };
-
-  const handleTranscriptionStop = () => {
-    // Send a request to the backend to stop transcription
-    fetch("http://localhost:3000/api/audio/stop-transcription", {
-      method: "POST",
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
-  };
+  const [transcriptions, setTranscriptions] = useState([]);
+  const socketRef = useRef(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/getData")
-      .then((response) => response.text())
-      .then((data) => {
-        setData(data);
+    // Initialize the socket connection
+    if (!socketRef.current) {
+      socketRef.current = io("http://localhost:3000");
+
+      socketRef.current.on("connect", () => {
+        setData(`You connected with id: ${socketRef.current.id}`);
       });
+
+      // Emit event to start transcription
+      socketRef.current.emit("startTranscription");
+
+      socketRef.current.on("transcription", (newTranscription) => {
+        setTranscriptions((prevTranscriptions) => [
+          ...prevTranscriptions,
+          newTranscription,
+        ]);
+      });
+    }
+
+    // disconnect the socket when the component unmounts
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
+    };
   }, []);
 
   return (
     <div>
       <p>{data}</p>
-      <button onClick={handleTranscriptionStart}>Start Transcription</button>
-      <button onClick={handleTranscriptionStop}>Stop Transcription</button>
+      <h2>Live Transcription</h2>
+      {transcriptions.map((transcription, index) => (
+        <p key={index}>{transcription}</p>
+      ))}
     </div>
   );
 }
