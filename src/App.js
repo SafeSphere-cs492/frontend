419ced67e09b5d18ad77540f330fcc834b23663d
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-03-29 14:15:17 -0400

    passing timestamp + shows when recording starts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { io } from "socket.io-client";
 function App() {
   const [data, setData] = useState("");
   const [transcriptions, setTranscriptions] = useState([]);
+  const [isRecording, setIsRecording] = useState(false);
   const socketRef = useRef(null);
 
   useEffect(() => {
@@ -17,15 +18,19 @@ function App() {
 
       socketRef.current.on(
         "transcriptionAnalysis",
-        ({ transcript, analysisResult }) => {
+        ({ transcript, analysisResult, timestamp }) => {
           console.log("transcript", transcript);
           console.log("analysisResult", analysisResult);
           setTranscriptions((prevTranscriptions) => [
             ...prevTranscriptions,
-            { text: transcript, analysis: analysisResult },
+            { text: transcript, analysis: analysisResult, timestamp },
           ]);
         }
       );
+
+      socketRef.current.on("recordingStatus", ({ recording }) => {
+        setIsRecording(recording);
+      });
     }
 
     // disconnect the socket when the component unmounts
@@ -37,23 +42,32 @@ function App() {
     };
   }, []);
 
+  const toggleTranscription = async () => {
+    if (isRecording) {
+      handleStopTranscription();
+    } else {
+      handleStartTranscription();
+    }
+  };
+
   const handleStartTranscription = () => {
     socketRef.current.emit("startTranscription");
   };
 
   const handleStopTranscription = () => {
     socketRef.current.emit("stopTranscription");
-    setTranscriptions([]);
   };
 
   return (
     <div>
       <p>{data}</p>
-      <button onClick={handleStartTranscription}>Start Transcription</button>
-      <button onClick={handleStopTranscription}>Stop Transcription</button>
+      <button onClick={toggleTranscription}>
+        {isRecording ? "Stop Transcription" : "Start Transcription"}
+      </button>
       <h2>Live Transcription</h2>
       {transcriptions.map((transcription, index) => (
         <div key={index}>
+          <p>{transcription.timestamp}</p>
           <p>{transcription.text}</p>
           <p>
             Analysis Score:{" "}
